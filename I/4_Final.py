f52a3e109c90c5343e92382a98b90e35f526048f
richardykedang <kedangrichard@gmail.com>
2021-03-13 16:04:27 +0700

    5

--- I/4_Final.py
+++ I/4_Final.py
@@ -4,3 +4,12 @@
 # 2 Ask the user which city did you grew up // jakarta
 # 3 Ask the user for the name pet? // Rabbit
 # Output  "Your band name could be ${city} ${pet}" //"Your band name could be Jakaart Rabbit"
+
+# solution
+# inputan
+city = input("Ask the user which city did you grew up ? ")
+pet = input("Ask the user for the name pet? ")
+
+# output
+print("Your band name could be {1} {0}".format(city, pet))
+print(f"Your band name could be {city} {pet}")
---
+++ I/5_DataType.py
@@ -0,0 +1,10 @@
+# String
+# print("Hello")
+# # Integer
+# print(1)
+# Float
+# print(2.5 + 2.5)
+# boolean
+# True/False
+# array []
+# print("hello"[4])
