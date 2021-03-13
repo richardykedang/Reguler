f417130de36763e972f826b72ac4619ce530f76d
richardykedang <kedangrichard@gmail.com>
2021-03-13 14:22:43 +0700

    Init Project

---
+++ I/1_Print.py
@@ -0,0 +1,10 @@
+# exercise 1
+# when you run program it should be :
+# Day 1 - Python print function
+# The function is declared like get_type_hints
+# print("what to print")
+
+print("hello world")
+print("Day 1 - Python print function")
+print("The function is declared like get_type_hints")
+print("print("what to print")")
